7e78d3f43134f223de30d65ca34fb5db6a316141
rubenabix <rubenabix@egmail.com>
2016-10-03 16:32:33 -0600

    refactor geometry topic

---
+++ src/app/features/topics/todo.js
@@ -0,0 +1,17 @@
+//TODO: remove
+
+
+// //'L 44.09830056250526 61.2214747707527 L 155.90169943749473 24.894348370484636 L 225 119.99999999999997z'
+// const A = snapRegularPolygons.text(155.90169943749476, 215.10565162951536, ['A']);
+// A.transform('t100,100r40,200,200');
+// snapRegularPolygons.text(44.09830056250526, 178.77852522924732, ['B']);
+//
+// snapRegularPolygons.text(44.09830056250526, 61.2214747707527, ['C']);
+// snapRegularPolygons.text(155.90169943749473, 24.894348370484636, ['D']);
+// snapRegularPolygons.text(225, 119.99999999999997, ['E']);
+
+/*    var w = angular.element($document[0].getElementById('svg'));
+ w.bind('resize', function () {
+ console.log('resize');
+ });
+ console.log(w);*/
